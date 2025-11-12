f00622ef8b31f20f41a892ecc8f82a41dfa71141
drclongstaff <drclongstaff@gmail.com>
2025-11-12 12:53:13 +0000

    Styling adjustments in spline function

--- Functions/Splin_calc.R
+++ Functions/Splin_calc.R
@@ -9,15 +9,15 @@ myPlate <- read.csv("Data/ClotLysisDoses.csv")
 
 
 allSpline <- function(S,  N, MIN, MAX, PLATE){
-Ts <- PLATE[[1]]
-aSpline <- function(S, TS, N, MIN, MAX){
-  maxx<-max(Ts)-MAX
-  DatSP <- spline(TS,S, N, MIN, maxx,   method = "natural") 
-        }
-DatSP <- map_df(PLATE[,-1], ~aSpline(.x, Ts, N, MIN, MAX))
-DatSPdf <- data.frame(split(DatSP$y,   ceiling(seq_along(DatSP$y) / N)))
-colnames(DatSPdf) <- colnames(PLATE[,-1])
-DatSPdf <- cbind("Ts"=DatSP$x[1:N], DatSPdf)
+  Ts <- PLATE[[1]]
+      aSpline <- function(S, TS, N, MIN, MAX){
+          maxx<-max(Ts)-MAX
+          DatSP <- spline(TS,S, N, MIN, maxx,   method = "natural") 
+                }
+    DatSP <- map_df(PLATE[,-1], ~aSpline(.x, Ts, N, MIN, MAX))
+    DatSPdf <- data.frame(split(DatSP$y,   ceiling(seq_along(DatSP$y) / N)))
+    colnames(DatSPdf) <- colnames(PLATE[,-1])
+    DatSPdf <- cbind("Ts"=DatSP$x[1:N], DatSPdf)
  
  }
 
