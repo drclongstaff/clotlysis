530869fe5288b730fe7e11572f01fb6ecf0878ab
drclongstaff <drclongstaff@gmail.com>
2025-12-29 17:12:40 +0000

    Tidy up function to plot a selected well

--- Functions/Plotone.R
+++ Functions/Plotone.R
@@ -1,14 +1,11 @@
-# This function plots data from all the wells
-one_plotFun <- function(PLATE, WELLNUM, TABRES) {
+# This function plots data from a selected well
+one_plotFun <- function(aPlate, wellNum, tabRes) {
   # Set plotting parameters
-  TabRes <- TABRES
-
-  #k <- WELLNUM
-  k<-which(colnames(PLATE)==WELLNUM)[1]-1
+  k<-which(colnames(aPlate)==wellNum)[1]-1
   par(mar = c(4, 4, 1, 1)) # dimensions for figure
-
+  maxy <- max(aPlate[[k+1]]) #in this case each plot keeps its max
   # Only one plot in this case so no looping needed
-  plotmake_fun(PLATE, TabRes, k, axx="s", axy="s")
+  plotmake_fun(aPlate, tabRes, k, axx="s", axy="s", maxy)
 }
 
 
